5009dac74aa7e32177efe500f795e499e4930741
cfclaudio <claudio.charmaine@gmail.com>
2022-01-16 19:10:58 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Coffee Roasters | Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Great coffee made simple.</main>
+
+      <footer>Coffee Roasters</footer>
+    </div>
+  );
+}
