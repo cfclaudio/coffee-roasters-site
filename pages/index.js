519880936392b2f8951bd38aac0a9a7f501b4979
cfclaudio <claudio.charmaine@gmail.com>
2022-01-16 19:45:52 -0500

    Set up theme-ui

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import React from "react";
 import Head from "next/head";
+import { Box, Paragraph } from "theme-ui";
 
 export default function Home() {
   return (
@@ -7,10 +9,8 @@ export default function Home() {
         <title>Coffee Roasters | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>Great coffee made simple.</main>
-
-      <footer>Coffee Roasters</footer>
+      <Box as="main">Great coffe made simple.</Box>
+      <Box as="footer">Coffee Roasters</Box>
     </div>
   );
 }
---
+++ styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: '"Avenir Next", sans-serif',
+    monospace: "Menlo, monospace",
+  },
+  colors: {
+    text: "#2C343E",
+    background: "#FEFCF7",
+    primary: "#0E8784", // teal
+    secondary: "#2C343E", // dark blue
+    accent: "#FDD6BA", // salmon
+    grey: "#83888F",
+    lightGrey: "#F4F1EB",
+  },
+};
